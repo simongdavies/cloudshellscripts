b621db6fa817a7e7ad09a03821077ee159017c3f
Simon Davies <sdavies@microsoft.com>
2018-02-09 11:45:09 +0000

    Added Moodle

---
+++ moodle/install-app.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euo pipefail
+
+# Login and get the Kubernetes Cluster credentials
+echo "az login --service-principal -u ${azprincipal} -p ${azsecret} --tenant ${aztenant}"
+az login --service-principal -u ${azprincipal} -p ${azsecret} --tenant ${aztenant}
+az aks get-credentials --resource-group ${resourcegroup} --name ${clustername}
+
+# Make sure that Helm is installed on the Kubernetes cluster
+
+helm init 
+
+# Wait for Helm (tiller) to be installed and running
+
+kubectl rollout status -w deployment/tiller-deploy --namespace=kube-system
+
+# Install Moodle
+
+helm install --set moodleUsername=${moodleusername},moodlePassword=${moodlepassword},moodleEmail=${moodleusername} stable/moodle
+
+
